26de898bcb7bcf9e3788fbde1139024382b288d3
Максим Найдович <max.naid@mail.ru>
2020-06-28 20:54:29 +0500

    add list

--- src/priceExample.js
+++ src/priceExample.js
@@ -4,7 +4,7 @@ export default {
     {
       id: '1',
       name: 'Пакет "Гриль"',
-      priceList: 0.35,
+      price: 0.35,
       packageAmount: 50,
       rate: 50,
       comment: 'Берите пока дешево'
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,8 +4,28 @@
       <div class="form-group">
         <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Введите пароль">
       </div>
-      <button @click="checkPass" class="btn btn-danger">Войти</button>
+      <button @click.prevent="checkPass" class="btn btn-danger">Войти</button>
     </form>
+    <table v-if="authorized" class="table table-sm table-striped">
+      <thead>
+      <tr>
+        <th scope="col">№</th>
+        <th scope="col">Название</th>
+        <th scope="col">Цена, шт.</th>
+        <th scope="col">В упак, шт</th>
+        <th scope="col">Цена упак</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(product, index) in getPrice.list" :key="product.id">
+        <th scope="row">{{index}}</th>
+        <td>{{product.name}}</td>
+        <td>{{product.price}}</td>
+        <td>{{product.packageAmount}}</td>
+        <td>{{getPackagePrice(product.price, product.packageAmount)}}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
@@ -14,7 +34,7 @@ import { mapGetters, mapMutations } from 'vuex'
 export default {
   name: 'Admin',
   data: () => ({
-    authorized: false
+    authorized: true
   }),
   computed: {
     ...mapGetters(['getPrice'])
@@ -28,6 +48,9 @@ export default {
       if (this.password === 'salam') {
         this.authorized = true
       }
+    },
+    getPackagePrice (price, amount) {
+      return price * amount
     }
   }
 }
