4a02aa893e6ddccede239a894fd9ce87ab10071d
Максим Найдович <max.naid@mail.ru>
2020-06-28 18:37:07 +0500

    add action

---
+++ src/priceExample.js
@@ -0,0 +1,13 @@
+export default {
+  category: 'Пакеты и Бытовые принадлежности',
+  list: [
+    {
+      id: '1',
+      name: 'Пакет "Гриль"',
+      price: 0.35,
+      packageAmount: 50,
+      rate: 50,
+      comment: 'Берите пока дешево'
+    }
+  ]
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import priceExample from '@/priceExample'
+const env = process.env.NODE_ENV
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    price: null
+  },
+  getters: {
+    getPrice (state) {
+      return state.price
+    }
   },
   mutations: {
+    initPrice (state, payload) {
+      state.price = payload
+    },
+    changePrice (state, payload) {
+      state.price = payload
+    }
   },
   actions: {
+    initPrice ({ commit }) {
+      let price = {}
+      if (env === 'development') {
+        price = priceExample
+      }
+      commit('initPrice', price)
+    },
+    changePrice ({ commit }, price) {
+      commit('changePrice', price)
+    }
   },
   modules: {
   }
